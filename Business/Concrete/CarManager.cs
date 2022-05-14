0fe76788e025e47451cb7f76a74a0b1404d9eab9
ozgun balta <ozgunbalta@Icloud.com>
2022-05-14 16:05:50 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,52 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void Add(Car car)
+        {
+            if (car.CarName.Length > 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+                Console.WriteLine("Araba Eklendi");
+            }
+            else if (car.CarName.Length < 2)
+            {
+                Console.WriteLine("Araba ismi 2 harften fazla olmalıdır.");
+            }
+            else
+            {
+                Console.WriteLine("Günlük fiyat 0' dan fazla olmalıdır.");
+            }
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(x => x.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(x => x.ColorId == id);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,40 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            //Car car1 = new Car { CarName = "Sabrina", BrandId = 1, ColorId = 1, Description = "Güzel Araba", ModelYear = 2021, DailyPrice = 150 };
+            //Car car2 = new Car { CarName = "Savorona", BrandId = 2, ColorId = 1, Description = "Fena Araba", ModelYear = 2022, DailyPrice = 380 };
+            //Car car3 = new Car { CarName = "Serkidoryan", BrandId = 2, ColorId = 2, Description = "Ama Ne Araba", ModelYear = 2022, DailyPrice = 450 };
+            //carManager.Add(car1);
+            //carManager.Add(car2);
+            //carManager.Add(car3);
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine($"Araba Adı: {car.CarName} Fiyatı:{car.DailyPrice}");
+            }
+            
+            Console.WriteLine("----------------------------------------------");
+
+            foreach (var car in carManager.GetCarsByColorId(1))
+            {
+                Console.WriteLine($"Araba Adı: {car.CarName} Fiyatı:{car.DailyPrice}");
+            }
+
+            Console.WriteLine("----------------------------------------------");
+
+            foreach (var car in carManager.GetCarsByBrandId(2))
+            {
+                Console.WriteLine($"Araba Adı: {car.CarName} Fiyatı:{car.DailyPrice}");
+            }
+        }
+    }
+}
